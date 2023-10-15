e80639feb66cec2a8aa53cff6f23f4e84c215fed
fantasyinfo <gs27349gs@gmail.com>
2023-10-15 18:19:21 +0530

    filter working

--- Activity-List/js/index.js
+++ Activity-List/js/index.js
@@ -1,42 +1,65 @@
-let menuOpenBtn = document.getElementById('menu');
-let menuCloseBtn = document.getElementById('close-menu');
-let menuBox = document.getElementsByClassName('desktop-menu');
+// menus
+let menuOpenBtn = document.getElementById("menu");
+let menuCloseBtn = document.getElementById("close-menu");
+let menuBox = document.getElementsByClassName("desktop-menu");
 
+menuOpenBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  menuBox[0].style.display = "block";
+});
 
-
-menuOpenBtn.addEventListener('click',(event)=>{
-    event.preventDefault();
-    menuBox[0].style.display = 'block';
-
-})
-
-menuCloseBtn.addEventListener('click',(event)=>{
-    event.preventDefault();
-    menuBox[0].style.display = 'none';
- 
+menuCloseBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  menuBox[0].style.display = "none";
 });
 
 
+// filters
+let closeFilterB = document.getElementById('clear_filter');
+let openFilterB = document.getElementById('filter_open');
+let filterBox = document.getElementsByClassName('filters');
 
-function changeColor(name, value){
-   let radio = document.querySelectorAll(`label[data-name="${name}"]`);
+openFilterB.addEventListener("click", (event) => {
+    event.preventDefault();
+    filterBox[0].style.display = "block";
+  });
+  
+  closeFilterB.addEventListener("click", (event) => {
+    event.preventDefault();
+    filterBox[0].style.display = "none";
+  });
 
-   radio.forEach((label)=>{
-    label.classList.remove('active');
-   })
-   let label =  document.querySelector(`label[for="${value}"]`);
-  label.classList.add('active')
+// apply filters
+function changeColor(name, value) {
+  let radio = document.querySelectorAll(`label[data-name="${name}"]`);
+
+  radio.forEach((label) => {
+    label.classList.remove("active");
+  });
+  let label = document.querySelector(`label[for="${value}"]`);
+
+  if (label) {
+    if (label.classList.contains("active")) {
+      label.classList.remove("active");
+    } else {
+      label.classList.add("active");
+    }
+  } else {
+    console.log(`Label for "${value}" not found.`);
+  }
 }
 
-function openFilter(id){
-    let getId = id.split('_')[1];
-    document.getElementsByClassName(`down-btn_${getId}`)[0].style.display = 'none';
-    document.getElementsByClassName(`up-btn_${getId}`)[0].style.display = 'block';
-    document.getElementById(id).classList.add('active');
+function openFilter(id) {
+  let getId = id.split("_")[1];
+  document.getElementsByClassName(`down-btn_${getId}`)[0].style.display =
+    "none";
+  document.getElementsByClassName(`up-btn_${getId}`)[0].style.display = "block";
+  document.getElementById(id).classList.add("active");
 }
-function closeFilter(id){
-    let getId = id.split('_')[1];
-    document.getElementsByClassName(`down-btn_${getId}`)[0].style.display = 'block';
-    document.getElementsByClassName(`up-btn_${getId}`)[0].style.display = 'none';
-    document.getElementById(id).classList.remove('active');
+function closeFilter(id) {
+  let getId = id.split("_")[1];
+  document.getElementsByClassName(`down-btn_${getId}`)[0].style.display =
+    "block";
+  document.getElementsByClassName(`up-btn_${getId}`)[0].style.display = "none";
+  document.getElementById(id).classList.remove("active");
 }
